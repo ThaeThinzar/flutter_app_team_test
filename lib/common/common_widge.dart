4bb59641f80d6bb36b9b8ffbf5fdd8bb9afd2041
Thae Thinzar <thaethinzarkhaingsoe@gmail.com>
2020-11-18 22:18:30 +0630

    [common_Kai] common page and function at dev

---
+++ lib/common/common_widge.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CommonWidget {
+  static Widget buttonWidget(BuildContext context, String screenName,String buttonText){
+    return Container(
+      margin: EdgeInsets.only(left: 8, right: 8),
+      width: MediaQuery.of(context).size.width,
+      child: RaisedButton(
+        onPressed: (){
+          onPressedToNavigate(screenName, context);
+        },
+        child: Text(buttonText),
+      ),
+    );
+  }
+  static void onPressedToNavigate(String screenName, BuildContext context){
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.of(context).pushNamed(screenName);
+    });
+  }
+  static void onReplacementScreen(String screenName, BuildContext context){
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.of(context).pushReplacementNamed(screenName);
+    });
+  }
+}
---
+++ lib/common/constants.dart
@@ -0,0 +1,4 @@
+class ScreenURLPath {
+  static const String PLATFORM_CHANNEL = '/plat_form_channel';
+  static const String SLIVER_TEST = '/sliver_test';
+}
